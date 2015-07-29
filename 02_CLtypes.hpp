4befd2a5e5ab4923332108f4be55e0abd51ad112
Jon Arintok <Jon.Arintok@Gmail.com>
2015-07-29 13:17:22 -0400

    improved '__pair' macro with const

--- 02_CLtypes.hpp
+++ 02_CLtypes.hpp
@@ -1,35 +1,43 @@
 
 typedef uint8_t  uchar;
 
+
 #define __pair(dt, st)\
 struct dt {\
   st x;\
   st y;\
   dt () : x(0), y(0) {}\
-  dt (st xIn, st yIn) : x(xIn), y(yIn) {}\
-  dt   operator +  (dt b) {return dt(x + b.x, y + b.y);}\
-  dt   operator -  (dt b) {return dt(x - b.x, y - b.y);}\
-  dt   operator *  (dt b) {return dt(x * b.x, y * b.y);}\
-  dt   operator /  (dt b) {return dt(x / b.x, y / b.y);}\
-  /*dt   operator %  (dt b) {return dt(x % b.x, y % b.y);}*/\
-  bool operator != (dt b) {return x != b.x  ||  y != b.y;}\
-  dt   operator +  (st b) {return dt(x + b, y + b);}\
-  dt   operator -  (st b) {return dt(x - b, y - b);}\
-  dt   operator *  (st b) {return dt(x * b, y * b);}\
-  dt   operator /  (st b) {return dt(x / b, y / b);}\
-  /*dt   operator %  (st b) {return dt(x % b, y % b);}*/\
-  bool operator == (st b) {return x == b  &&  y == b;}\
-  bool operator != (st b) {return x != b  ||  y != b;}\
-  void operator += (dt b) {x += b.x; y += b.y;}\
-  void operator -= (dt b) {x -= b.x; y -= b.y;}\
-  void operator *= (dt b) {x *= b.x; y *= b.y;}\
-  void operator /= (dt b) {x /= b.x; y /= b.y;}\
-  void operator += (st b) {x += b; y += b;}\
-  void operator -= (st b) {x -= b; y -= b;}\
-  void operator *= (st b) {x *= b; y *= b;}\
-  void operator /= (st b) {x /= b; y /= b;}\
-  void operator  = (st b) {x  = b; y  = b;}\
+  dt (const st xIn, const st yIn) : x(xIn), y(yIn) {}\
+  dt operator +  (const dt b) {return dt(x + b.x, y + b.y);}\
+  dt operator -  (const dt b) {return dt(x - b.x, y - b.y);}\
+  dt operator *  (const dt b) {return dt(x * b.x, y * b.y);}\
+  dt operator /  (const dt b) {return dt(x / b.x, y / b.y);}\
+  bool   operator == (const dt b) {return x == b.x  &&  y == b.y;}\
+  bool   operator != (const dt b) {return x != b.x  ||  y != b.y;}\
+  void   operator += (const dt b) {x += b.x; y += b.y;}\
+  void   operator -= (const dt b) {x -= b.x; y -= b.y;}\
+  void   operator *= (const dt b) {x *= b.x; y *= b.y;}\
+  void   operator /= (const dt b) {x /= b.x; y /= b.y;}\
+  dt operator +  (const st  b) {return dt(x + b, y + b);}\
+  dt operator -  (const st  b) {return dt(x - b, y - b);}\
+  dt operator *  (const st  b) {return dt(x * b, y * b);}\
+  dt operator /  (const st  b) {return dt(x / b, y / b);}\
+  bool   operator == (const st  b) {return x == b  &&  y == b;}\
+  bool   operator != (const st  b) {return x != b  ||  y != b;}\
+  void   operator += (const st  b) {x += b; y += b;}\
+  void   operator -= (const st  b) {x -= b; y -= b;}\
+  void   operator *= (const st  b) {x *= b; y *= b;}\
+  void   operator /= (const st  b) {x /= b; y /= b;}\
+  void   operator  = (const st  b) {x  = b; y  = b;}\
+  bool   operator !  () {return !x && !y;}\
   st pro() {return x*y;}\
+  st sel(uint8_t axis) {\
+    switch(axis) {\
+      case  0: return x;\
+      case  1: return y;\
+      default: return 0;\
+    }\
+  }\
 };
 
 __pair(int2, int);
